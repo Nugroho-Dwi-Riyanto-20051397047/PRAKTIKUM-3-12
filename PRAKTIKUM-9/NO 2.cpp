b9069e65510b05e09d5301dce474c75338d53d81
Nugroho Dwi Riyanto (20051397047) <72641363+Nugroho-Dwi-Riyanto-20051397047@users.noreply.github.com>
2021-06-07 21:23:26 +0700

    Update NO 2.cpp

--- PRAKTIKUM-9/NO 2.cpp
+++ PRAKTIKUM-9/NO 2.cpp
@@ -43,9 +43,9 @@ int main()
   int arr[max];
    int i,n,lb,ub;
    lb=0;
-   printf("RIZKI JANUAR IRMANSYAH\n");
- printf("20051397046\n");
- printf("MI_B_2020\n");
+   printf("NUGROHO DWI RIYANTO\n");
+ printf("20051397047\n");
+ printf("MI_A_2020\n");
    cout<<"\tQUICK SORT\n\n";
    cout<<"Masukkan jumlah index : ";
    cin>>n;
